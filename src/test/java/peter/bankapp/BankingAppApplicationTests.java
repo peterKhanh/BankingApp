6ec1ddb7242c89e6e8eeac1af5b1e4d82a739ce8
peterKhanh <tkhn2020@gmail.com>
2025-05-13 16:21:20 +0700

    Add java Spring boot to git

---
+++ src/test/java/peter/bankapp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package peter.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
